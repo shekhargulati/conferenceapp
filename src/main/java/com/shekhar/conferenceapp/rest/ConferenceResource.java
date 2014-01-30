89d06c3ccd74b8a5ebbc1605ff269cf01350bf81
Shekhar Gulati <shekhargulati84@gmail.com>
2014-01-31 04:54:29 +0530

    step 3: made conference resource Async

--- src/main/java/com/shekhar/conferenceapp/rest/ConferenceResource.java
+++ src/main/java/com/shekhar/conferenceapp/rest/ConferenceResource.java
@@ -2,12 +2,16 @@ package com.shekhar.conferenceapp.rest;
 
 import java.util.logging.Logger;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -23,17 +27,29 @@ public class ConferenceResource {
 
     @Inject
     private Logger logger;
+    
+    @Resource
+    private ManagedExecutorService mes;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createNewConference(@Valid final Conference conference) {
-        logger.info("Saving conference .. " + conference);
-        Conference persitedConference = conferenceService.save(conference);
-        logger.info("Saved conference ..");
-        return Response.status(Status.CREATED).entity(persitedConference).build();
+    public void createNewConference(@Suspended final AsyncResponse asyncResponse, @Valid final Conference conference) {
+        mes.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    // ignoring it
+                }
+                logger.info("Saving conference .. " + conference);
+                Conference persitedConference = conferenceService.save(conference);
+                logger.info("Saved conference ..");
+                asyncResponse.resume(Response.status(Status.CREATED).entity(persitedConference).build());
+            }
+        });
 
     }
 
-    
 }
